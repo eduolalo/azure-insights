75cf48a284643b4004561cf03c7ffba9fcf6a9ff
Eduardo <rcchristiane@gmail.com>
2022-11-17 09:51:31 -0500

    insigst no sirven

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/kalmecak/azure-insights
 go 1.18
 
 require (
+	github.com/microsoft/ApplicationInsights-Go v0.4.4
 	go.opentelemetry.io/otel v1.11.1
-	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.11.1
-	go.opentelemetry.io/otel/sdk v1.11.1
 	go.opentelemetry.io/otel/trace v1.11.1
 )
 
 require (
+	code.cloudfoundry.org/clock v1.0.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	github.com/gofrs/uuid v4.3.1+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -1,88 +1,19 @@
 package main
 
 import (
-	"context"
-	"io"
-	"log"
 	"os"
-	"os/signal"
 
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	"github.com/microsoft/ApplicationInsights-Go/appinsights"
 )
 
-// newExporter returns a console exporter.
-func newExporter(w io.Writer) (trace.SpanExporter, error) {
-	return stdouttrace.New(
-		stdouttrace.WithWriter(w),
-		// Use human-readable output.
-		stdouttrace.WithPrettyPrint(),
-		// Do not print timestamps for the demo.
-		stdouttrace.WithoutTimestamps(),
-	)
-}
-
-// newResource returns a resource describing this application.
-func newResource() *resource.Resource {
-	r, _ := resource.Merge(
-		resource.Default(),
-		resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceNameKey.String("fib"),
-			semconv.ServiceVersionKey.String("v0.1.0"),
-			attribute.String("environment", "La perra locochona"),
-		),
-	)
-	return r
-}
+var telemetryClient appinsights.TelemetryClient
+var key string = os.Getenv("APP_INSIGHTS_KEY")
 
 func main() {
-	l := log.New(os.Stdout, "", 0)
 
-	// Write telemetry data to a file.
-	f, err := os.Create("telemetry.log")
-	if err != nil {
-		l.Fatal(err)
-	}
-	defer f.Close()
-
-	// Create a console exporter.
-	exp, err := newExporter(f)
-	if err != nil {
-		l.Fatal(err)
-	}
-
-	tp := trace.NewTracerProvider(
-		trace.WithBatcher(exp),
-		trace.WithResource(newResource()),
-	)
-	defer func() {
-		if err := tp.Shutdown(context.Background()); err != nil {
-			l.Fatal(err)
-		}
-	}()
-	otel.SetTracerProvider(tp)
-
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt)
-
-	errCh := make(chan error)
-	app := NewApp(os.Stdin, l)
-	go func() {
-		errCh <- app.Run(context.Background())
-	}()
-
-	select {
-	case <-sigCh:
-		l.Println("\ngoodbye")
-		return
-	case err := <-errCh:
-		if err != nil {
-			l.Fatal(err)
-		}
-	}
+	config := appinsights.NewTelemetryConfiguration(key)
+	config.EndpointUrl = "https://southcentralus-3.in.applicationinsights.azure.com/"
+	telemetryClient = appinsights.NewTelemetryClientFromConfig(config)
+	telemetryClient.Context().Tags.Cloud().SetRole("Moldava")
+	telemetryClient.TrackTrace("Hello World!", appinsights.Information)
 }
